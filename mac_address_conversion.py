f7198736743a3b824ca102a90bdeec5981678934
Dmitry Golovach <dmitry.golovach@outlook.com>
2020-02-26 22:52:22 -0600

    change variable names

--- mac_address_conversion.py
+++ mac_address_conversion.py
@@ -2,14 +2,14 @@ import re
 import sys
 
 
-def mac_normalization(current_mac_address, symbol):
+def mac_conversion(normalized_mac_address, symbol):
     if symbol == '.':
-        return symbol.join([current_mac_address[i:i + 4] for i in range(0, len(current_mac_address), 4)])
-    return symbol.join(a + b for a, b in zip(current_mac_address[::2], current_mac_address[1::2]))
+        return symbol.join([normalized_mac_address[i:i + 4] for i in range(0, len(normalized_mac_address), 4)])
+    return symbol.join(a + b for a, b in zip(normalized_mac_address[::2], normalized_mac_address[1::2]))
 
 
 def main(current_mac_address, mac_address_format):
-    current_mac_address = re.sub(r'\W+', '', current_mac_address)
+    normalized_mac_address = re.sub(r'\W+', '', current_mac_address)
 
     dict_mac_formats = {
         'none': '',
@@ -21,9 +21,9 @@ def main(current_mac_address, mac_address_format):
     if symbol == 'Invalid format':
         return 'Invalid format. Should be none | dot |  colon | dash '
 
-    if len(current_mac_address) == 12:
+    if len(normalized_mac_address) == 12:
         symbol = dict_mac_formats.get(mac_address_format, 'Invalid format')
-        return mac_normalization(current_mac_address, symbol)
+        return mac_conversion(normalized_mac_address, symbol)
     else:
         return "Invalid format. Should be 01-23-45-67-89-AB | 01:23:45:67:89:AB | 0123456789AB | 0123.4567.89AB or " \
                "any combination "
