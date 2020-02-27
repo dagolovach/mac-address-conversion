2e29d99774efd729fe98a2fe68041c9e4283df29
Dmitry Golovach <dmitry.golovach@outlook.com>
2020-02-26 20:05:32 -0600

    commit

--- mac_address_conversion.py
+++ mac_address_conversion.py
@@ -1,19 +1,21 @@
 import re
+import sys
+
 
 def mac_normalization(current_mac_address, symbol):
     if symbol == '.':
-        return symbol.join([current_mac_address[i:i+4] for i in range(0, len(current_mac_address), 4)])
+        return symbol.join([current_mac_address[i:i + 4] for i in range(0, len(current_mac_address), 4)])
     return symbol.join(a + b for a, b in zip(current_mac_address[::2], current_mac_address[1::2]))
 
-def main(current_mac_address, mac_address_format):
 
+def main(current_mac_address, mac_address_format):
     current_mac_address = re.sub(r'\W+', '', current_mac_address)
 
     dict_mac_formats = {
-        'mac_address_pure': '',
-        'mac_address_dot': '.',
-        'mac_address_colon': ':',
-        'mac_address_dash': '-'
+        'none': '',
+        'dot': '.',
+        'colon': ':',
+        'dash': '-'
     }
 
     if len(current_mac_address) == 12:
@@ -28,15 +30,6 @@ def main(current_mac_address, mac_address_format):
 
 
 if __name__ == '__main__':
-    """
-    ccef.4870.c081
-    cc-ef-48-70-c0-81
-    cc:ef:48:70:c0:81
-    ccef4870c081   
-    """
-    current_mac_address = 'cc:ef:48:70:c0:84'
-    mac_address_format = 'mac_address_pure'
-    #mac_address_format = 'mac_address_colon'
-    #mac_address_format = 'mac_address_dash'
-    #mac_address_format = 'mac_address_pure'
-    print(main(current_mac_address, mac_address_format))
+    if len(sys.argv) != 3:
+        raise SyntaxError("Insufficient arguments.")
+    main(sys.argv[1], sys.argv[2])
